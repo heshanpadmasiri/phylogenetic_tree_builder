fb14eed47a88ffaf5ee9251bd6496ae07ea6c16f
Heshan <hpheshan@gmail.com>
2021-03-17 18:48:27 +0530

    added tree builder

--- makefile
+++ makefile
@@ -1,3 +1,6 @@
+output/helix-turn-helix_transcriptional_regulator_tree.nex: output/helix-turn-helix_transcriptional_regulator_alignment.fasta
+	python src/tree_builder.py --silent True output/helix-turn-helix_transcriptional_regulator_alignment.fasta
+
 output/helix-turn-helix_transcriptional_regulator_alignment.fasta: output/helix-turn-helix_transcriptional_regulator.fasta
 	./clustalo-1.2.4-Ubuntu-x86_64 -i output/helix-turn-helix_transcriptional_regulator.fasta --outfile output/helix-turn-helix_transcriptional_regulator_alignment.fasta
 
---
+++ src/tree_builder.py
@@ -0,0 +1,34 @@
+from Bio import Phylo
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+from Bio import AlignIO
+import argparse
+
+
+def build_tree(alignment_file: str) -> Phylo.BaseTree.Tree:
+    alignemt = AlignIO.read(alignment_file, 'fasta')
+    calculator = DistanceCalculator('identity')
+    tree_counstructor = DistanceTreeConstructor()
+    tree = tree_counstructor.upgma(calculator.get_distance(alignemt))
+    return tree
+
+
+def to_nexus_file(tree: Phylo.BaseTree.Tree, output_name: str) -> None:
+    Phylo.write([tree], f'{output_name}_tree.nex', 'nexus')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Create phylogenetic tree using the alignment output")
+    parser.add_argument('alignment_file',
+                        type=str,
+                        help='FASTA alignment file from clustal Omega')
+    parser.add_argument('--silent',
+                        type=bool,
+                        help="Don't show any outputs",
+                        default=False)
+    args = parser.parse_args()
+    tree = build_tree(args.alignment_file)
+    if not args.silent:
+        Phylo.draw_ascii(tree)
+    to_nexus_file(tree, args.alignment_file.strip('_alignment.fasta'))
